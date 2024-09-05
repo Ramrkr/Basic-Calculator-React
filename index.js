20998afc933a64fb853ee4311080ef30d3fc46e7
Ramrkr <126065521+Ramrkr@users.noreply.github.com>
2024-09-05 11:23:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const res=document.getElementById("result");
+
+
+function insert(value){
+  if (!res.value){
+    res.value="";
+  }
+  res.value+=value;
+  console.log(res.value)
+}
+function calc(value){
+  const calculate=eval(value||null);
+  if (isNaN(calculate)) {
+    res.value = "Can't divide 0 with 0";
+    setTimeout(() => {
+      res.value = "";
+    }, 1300);
+  } else {
+    res.value = calculate;
+  }
+
+}
+// Example usage:
+// Call handleButtonClick('+') when the '+' button is clicked
+// Call handleButtonClick('5') when the '5' button is clicked
